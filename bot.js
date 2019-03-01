60a6c16d92af2d33c974fa30d205b0d4c17c9536
No Nema <44880953+mrdahom4@users.noreply.github.com>
2019-03-01 06:31:37 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,6 +41,18 @@ client.on('ready', () => {
   console.log('')
 });
 
+client.on("message", message => {
+    if (message.content.startsWith(prefix + "bc")) {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+  m.send(`${argresult}\n ${m}`);
+  })
+  message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  عدد المستلمين `);
+  message.delete();
+  };
+  });
 
 client.on('message',async message => {
   if(message.content.startsWith(prefix + "voice")) {
